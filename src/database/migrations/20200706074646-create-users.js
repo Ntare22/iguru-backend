6bcd79fd0029510d443c2776f0ba94dbe285b115
Izabayo Blaise <52036624+blaise82@users.noreply.github.com>
2020-07-05 00:20:00 +0200

    ch(setup): integrate traviCI
    - create travic.yml file
    
    [starts #173614690]

---
+++ src/database/migrations/20200706074646-create-users.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      names: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      verified: {
+        type: Sequelize.BOOLEAN
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      company: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,12 +11,11 @@ const app = express();
 
 // Log requests to the console.
 app.use(logger('dev'));
+app.use(cors());
 
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) =>
   res.status(200).send({
     message: 'Welcome to IGURU.',
