cb6d45d3505e78eb98065da1eda7e2b7c70887a2
Izabayo Blaise <52036624+blaise82@users.noreply.github.com>
2020-07-05 14:53:07 +0200

    ch(setup): setup sequelize
    - install pg
    - install sequelize
    - config sequelize
    
    [Starts #173614659]

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import express from 'express';
+import logger from 'morgan';
 import bodyParser from 'body-parser';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Set up the express app
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('example route');
-});
+// Log requests to the console.
+app.use(logger('dev'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) =>
+  res.status(200).send({
+    message: 'Welcome to IGURU.',
+  })
+);
 
 app.listen(process.env.PORT || 8080, () =>
-  console.log('app listening on port 3000!')
+  console.log(`app listening on port ${process.env.PORT}!`)
 );
 
 export default app;
